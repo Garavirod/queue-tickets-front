aa777c347443f5867dda2d7870865bb4526cc5cb
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-12-02 15:09:04 -0600

    Component Create Ticket was generated

--- src/pages/CreateTicket.jsx
+++ src/pages/CreateTicket.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import { Row, Col, Button, Typography } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+
+const {Title, Text} = Typography;
 
 export const CreateTicket = () =>{
+
+
+    const newTicket = () => {
+
+    }
+
     return(
-        <div>Create ticket</div>
+        <React.Fragment>          
+            <Row>
+                <Col span={14} offset={6} align="center">
+                    <Title level={3}>
+                        Push for generating a new ticket
+                    </Title>
+                    <Button 
+                        type="primary"
+                        shape="round"
+                        icon={<DownloadOutlined />}
+                        size="large"
+                        onClick={newTicket}
+                    >                        
+                        Generate
+                    </Button>
+                </Col>
+            </Row>       
+            <Row style={{marginTop:100}}>
+                <Col span={14} offset={6} align="center">
+                    <Text level={2}>Your number is :</Text>
+                    <br/>
+                    <Text 
+                        type="success"
+                        style={{fontSize:55}}
+                    >
+                        56
+                    </Text>
+                </Col>
+            </Row>     
+        </React.Fragment>
     )
 }
